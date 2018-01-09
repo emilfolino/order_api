ac3cc37d03ba3245367ec1d6d5b20ad6a8a98543
Emil Folino <emil.folino@bth.se>
2018-01-09 07:56:18 +0100

    Added CORS and changed docs to mirror api

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const app = express()
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const port = 1337
 
 app.all('*', checkAPIKey)
