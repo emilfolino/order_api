5ce43f0b65d9baefe9a0cea6c9027e490e9e9b4b
Emil Folino BTH MAC <emilfolino@gmail.com>
2018-01-15 13:40:26 +0100

    added orders, order_items and transactions in db

--- db/migrate.sql
+++ db/migrate.sql
@@ -15,3 +15,36 @@ CREATE TABLE IF NOT EXISTS products (
     FOREIGN KEY(apiKey) REFERENCES apikeys(key),
     UNIQUE(productId, apiKey)
 );
+
+CREATE TABLE IF NOT EXISTS orders (
+    orderId INTEGER NOT NULL,
+    customerName VARCHAR(255),
+    customerAddress VARCHAR(255),
+    customerZip VARCHAR(12),
+    customerCity VARCHAR(255),
+    customerCountry VARCHAR(255),
+    apiKey VARCHAR(32) NOT NULL,
+    FOREIGN KEY(apiKey) REFERENCES apikeys(key),
+    UNIQUE(orderId, apiKey)
+);
+
+CREATE TABLE IF NOT EXISTS order_items (
+    orderItemId INTEGER NOT NULL,
+    orderId INTEGER NOT NULL,
+    productId INTEGER NOT NULL,
+    amount INTEGER NOT NULL,
+    apiKey VARCHAR(32) NOT NULL,
+    FOREIGN KEY(apiKey) REFERENCES apikeys(key),
+    FOREIGN KEY(orderId) REFERENCES orders(orderId),
+    FOREIGN KEY(productId) REFERENCES products(productId),
+    UNIQUE(orderItemId, apiKey)
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+    productId INTEGER NOT NULL,
+    amount INTEGER NOT NULL,
+    message VARCHAR(255),
+    apiKey VARCHAR(32) NOT NULL,
+    FOREIGN KEY(apiKey) REFERENCES apikeys(key),
+    FOREIGN KEY(productId) REFERENCES products(productId)
+);
--- db/seed.sql
+++ db/seed.sql
@@ -5,3 +5,11 @@ INSERT INTO products (productId, articleNumber, productName, productDescription,
 INSERT INTO products (productId, articleNumber, productName, productDescription, productSpecifiers, stock, location, apiKey) VALUES (2, "1212-TNT", "Bolt 14mm", "A bolt that fits the mighty fine screw.", "{length : '5mm', width : '5mm'}", 15, "A1C4", "fdc42b2d941e8c6f7b38d974df3758ce");
 INSERT INTO products (productId, articleNumber, productName, productDescription, productSpecifiers, stock, location, apiKey) VALUES (1, "QRT-LLL-14", "Blue yarn", "Nice quality yarn.", "{ color : 'blue', thickness : 8}", 2, "B14", "1bdd5c943e664d399f2a298fca0dabfd");
 INSERT INTO products (productId, articleNumber, productName, productDescription, productSpecifiers, stock, location, apiKey) VALUES (3, "QRT-R34-14", "Red yarn", "Low qaulity yarn.", "{ color : 'red', thickness : 2}", 3, "C12", "1bdd5c943e664d399f2a298fca0dabfd");
+
+INSERT INTO orders (orderId, customerName, customerAddress, customerZip, customerCity, customerCountry, apiKey) VALUES (1, "Anders Andersson", "Andersgatan 1", "12345", "Anderstorp", "Sweden", "fdc42b2d941e8c6f7b38d974df3758ce");
+INSERT INTO order_items (orderItemId, orderId, productId, amount, apiKey) VALUES (1, 1, 1, 2, "fdc42b2d941e8c6f7b38d974df3758ce");
+INSERT INTO order_items (orderItemId, orderId, productId, amount, apiKey) VALUES (2, 1, 2, 3, "fdc42b2d941e8c6f7b38d974df3758ce");
+
+INSERT INTO orders (orderId, customerName, customerAddress, customerZip, customerCity, customerCountry, apiKey) VALUES (1, "Bengt Bengtsson", "Bengtsgatan 2", "54321", "Bengtsfors", "Sweden", "1bdd5c943e664d399f2a298fca0dabfd");
+INSERT INTO order_items (orderItemId, orderId, productId, amount, apiKey) VALUES (1, 1, 1, 2, "1bdd5c943e664d399f2a298fca0dabfd");
+INSERT INTO order_items (orderItemId, orderId, productId, amount, apiKey) VALUES (2, 1, 3, 3, "1bdd5c943e664d399f2a298fca0dabfd");
