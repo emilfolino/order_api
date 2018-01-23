c22be141085b9548d01b626ece27dea1849a79de
Emil Folino <emil.folino@bth.se>
2018-01-23 09:08:00 +0100

    Added logging and further dev-depencies for testing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const morgan = require('morgan');
 
 const auth = require('./models/auth.js');
 
@@ -11,6 +12,12 @@ const orderItems = require('./models/order_items.js');
 const app = express();
 
 const port = 8111;
+
+//don't show the log when it is test
+if (process.env.NODE_ENV !== 'test') {
+    //use morgan to log at command line
+    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+}
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
