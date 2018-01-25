b7665b5a174e5ca6a1a53ec24b9c83a1bf462f7b
Emil Folino <emil.folino@bth.se>
2018-01-25 08:24:57 +0100

    Added more tests orders and order_items

---
+++ test/order_items.js
@@ -0,0 +1,329 @@
+process.env.NODE_ENV = 'test';
+
+//Require the dev-dependencies
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app.js');
+const should = chai.should();
+
+const db = require("../db/database.js");
+
+chai.use(chaiHttp);
+
+let apiKey = "";
+
+
+describe('order items', () => {
+    before(() => {
+        db.run("DELETE FROM apiKeys", (err) => {
+            if (err) {
+                console.log("Could not empty test DB table apiKeys", err.message);
+            }
+        });
+
+        db.run("DELETE FROM products", (err) => {
+            if (err) {
+                console.log("Could not empty test DB table orders", err.message);
+            }
+        });
+
+        db.run("DELETE FROM orders", (err) => {
+            if (err) {
+                console.log("Could not empty test DB table orders", err.message);
+            }
+        });
+
+        db.run("DELETE FROM order_items", (err) => {
+            if (err) {
+                console.log("Could not empty test DB table orders", err.message);
+            }
+        });
+    });
+
+    describe('POST /order_item', () => {
+        it('should get 401 as we do not provide valid api_key', (done) => {
+            let order_item = {
+                order_id: 1,
+                product_id: 1,
+                amount: 1,
+                api_key: apiKey
+            };
+
+            chai.request(server)
+                .post("/order_item")
+                .send(order_item)
+                .end((err, res) => {
+                    res.should.have.status(401);
+                    res.body.should.be.an("object");
+                    res.body.errors.status.should.be.equal(401);
+                    done();
+                });
+        });
+
+        it('should get 200 HAPPY PATH FROM GETTING API KEY', (done) => {
+            chai.request(server)
+                .get("/api_key?email=test@order.com")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an("object");
+                    res.body.data.should.be.an("object");
+                    res.body.data.should.have.property("key");
+
+                    apiKey = res.body.data.key;
+
+                    done();
+                });
+        });
+
+
+        it('should get 400 as we do not supply order id', (done) => {
+            let order_item = {
+                // order_id: 1,
+                product_id: 1,
+                amount: 1,
+                api_key: apiKey
+            };
+
+            chai.request(server)
+                .post("/order_item")
+                .send(order_item)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("errors");
+                    res.body.errors.should.have.property("status");
+                    res.body.errors.status.should.be.equal(400);
+                    res.body.errors.should.have.property("detail");
+
+                    done();
+                });
+        });
+
+        it('should get 400 as we do not supply product id', (done) => {
+            let order_item = {
+                order_id: 1,
+                // product_id: 1,
+                amount: 1,
+                api_key: apiKey
+            };
+
+            chai.request(server)
+                .post("/order_item")
+                .send(order_item)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("errors");
+                    res.body.errors.should.have.property("status");
+                    res.body.errors.status.should.be.equal(400);
+                    res.body.errors.should.have.property("detail");
+
+                    done();
+                });
+        });
+
+        it('should get 200 HAPPY PATH', (done) => {
+            let order = {
+                id: 1,
+                name: "Anders",
+                api_key: apiKey
+            };
+
+            chai.request(server)
+                .post("/order")
+                .send(order)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("data");
+
+                    done();
+                });
+        });
+
+        it('should get 200 HAPPY PATH', (done) => {
+            let product = {
+                id: 1,
+                name: "Screw",
+                description: "Mighty fine screw.",
+                api_key: apiKey
+            };
+
+            chai.request(server)
+                .post("/product")
+                .send(product)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("data");
+
+                    done();
+                });
+        });
+
+        it('should get 200 HAPPY PATH', (done) => {
+            let order_item = {
+                order_id: 1,
+                product_id: 1,
+                amount: 1,
+                api_key: apiKey
+            };
+
+            chai.request(server)
+                .post("/order_item")
+                .send(order_item)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("data");
+
+                    done();
+                });
+        });
+
+        it('should get 200 HAPPY PATH', (done) => {
+            chai.request(server)
+                .get("/order/1?api_key=" + apiKey)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an("object");
+                    res.body.data.should.be.an("object");
+                    res.body.data.should.have.property("id");
+                    res.body.data.id.should.be.equal(1);
+                    res.body.data.order_items.should.be.an("array");
+                    res.body.data.order_items.length.should.be.equal(1);
+
+                    done();
+                });
+        });
+    });
+
+    describe('PUT /order_item', () => {
+        it('should get 400 no order id supplied', (done) => {
+            let order_item = {
+                // order_id: 1,
+                product_id: 1,
+                amount: 2,
+                api_key: apiKey
+            };
+
+            chai.request(server)
+            .put("/order_item?api_key=" + apiKey)
+            .send(order_item)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.an("object");
+                res.body.should.have.property("errors");
+                res.body.errors.should.have.property("status");
+                res.body.errors.status.should.be.equal(400);
+                res.body.errors.should.have.property("detail");
+
+                done();
+            });
+        });
+
+        it('should get 400 no product id supplied', (done) => {
+            let order_item = {
+                order_id: 1,
+                // product_id: 1,
+                amount: 2,
+                api_key: apiKey
+            };
+
+            chai.request(server)
+            .put("/order_item?api_key=" + apiKey)
+            .send(order_item)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.an("object");
+                res.body.should.have.property("errors");
+                res.body.errors.should.have.property("status");
+                res.body.errors.status.should.be.equal(400);
+                res.body.errors.should.have.property("detail");
+
+                done();
+            });
+        });
+
+        it('should get 200 HAPPY PATH', (done) => {
+            let order_item = {
+                order_id: 1,
+                product_id: 1,
+                amount: 2,
+                api_key: apiKey
+            };
+
+            chai.request(server)
+                .post("/order_item")
+                .send(order_item)
+                .end((err, res) => {
+                    res.should.have.status(204);
+
+                    done();
+                });
+        });
+
+        // it('should get 200, with changed name and description', (done) => {
+        //     chai.request(server)
+        //     .get("/order/1?api_key=" + apiKey)
+        //     .end((err, res) => {
+        //         res.should.have.status(200);
+        //         res.body.data.should.have.property("name");
+        //         res.body.data.name.should.be.equal("Bengt");
+        //
+        //         done();
+        //     });
+        // });
+    });
+    //
+    // describe('DELETE /order', () => {
+    //     it('should get 400 no id supplied', (done) => {
+    //         let order = {
+    //             api_key: apiKey
+    //         };
+    //
+    //         chai.request(server)
+    //         .delete("/order?api_key=" + apiKey)
+    //         .send(order)
+    //         .end((err, res) => {
+    //             res.should.have.status(400);
+    //             res.body.should.be.an("object");
+    //             res.body.should.have.property("errors");
+    //             res.body.errors.should.have.property("status");
+    //             res.body.errors.status.should.be.equal(400);
+    //             res.body.errors.should.have.property("detail");
+    //
+    //             done();
+    //         });
+    //     });
+    //
+    //     it('should get 204 HAPPY PATH', (done) => {
+    //         let order = {
+    //             id: 1,
+    //             api_key: apiKey
+    //         };
+    //
+    //         chai.request(server)
+    //         .delete("/order")
+    //         .send(order)
+    //         .end((err, res) => {
+    //             res.should.have.status(204);
+    //
+    //             done();
+    //         });
+    //     });
+    //
+    //     it('should get 200 HAPPY PATH getting no orders', (done) => {
+    //         chai.request(server)
+    //         .get("/orders?api_key=" + apiKey)
+    //         .end((err, res) => {
+    //             res.should.have.status(200);
+    //             res.body.should.be.an("object");
+    //             res.body.data.should.be.an("array");
+    //             res.body.data.length.should.be.equal(0);
+    //
+    //             done();
+    //         });
+    //     });
+    // });
+});
--- test/orders.js
+++ test/orders.js
@@ -199,137 +199,133 @@ describe('orders', () => {
                 });
         });
     });
-    //
-    // describe('GET /product/search/:query', () => {
-    //     it('should get 200 HAPPY PATH', (done) => {
-    //         chai.request(server)
-    //             .get("/product/search/screw?api_key=" + apiKey)
-    //             .end((err, res) => {
-    //                 res.should.have.status(200);
-    //                 res.body.should.be.an("object");
-    //                 res.body.data.should.be.an("array");
-    //                 res.body.data.length.should.be.equal(1);
-    //
-    //                 done();
-    //             });
-    //     });
-    //
-    //     it('should get 200, but empty data object', (done) => {
-    //         chai.request(server)
-    //             .get("/product/search/bolt?api_key=" + apiKey)
-    //             .end((err, res) => {
-    //                 res.should.have.status(200);
-    //                 res.body.data.should.be.eql([]);
-    //
-    //                 done();
-    //             });
-    //     });
-    // });
-    //
-    // describe('PUT /product', () => {
-    //     it('should get 400 no id supplied', (done) => {
-    //         let product = {
-    //             name: "Big Screw",
-    //             description: "Mighty fine big screw.",
-    //             api_key: apiKey
-    //         };
-    //
-    //         chai.request(server)
-    //             .put("/product?api_key=" + apiKey)
-    //             .send(product)
-    //             .end((err, res) => {
-    //                 res.should.have.status(400);
-    //                 res.body.should.be.an("object");
-    //                 res.body.should.have.property("errors");
-    //                 res.body.errors.should.have.property("status");
-    //                 res.body.errors.status.should.be.equal(400);
-    //                 res.body.errors.should.have.property("detail");
-    //
-    //                 done();
-    //             });
-    //     });
-    //
-    //     it('should get 204 HAPPY PATH', (done) => {
-    //         let product = {
-    //             id: 1,
-    //             name: "Big Screw",
-    //             description: "Mighty fine big screw.",
-    //             api_key: apiKey
-    //         };
-    //
-    //         chai.request(server)
-    //             .put("/product?api_key=" + apiKey)
-    //             .send(product)
-    //             .end((err, res) => {
-    //                 res.should.have.status(204);
-    //
-    //                 done();
-    //             });
-    //     });
-    //
-    //     it('should get 200, with changed name and description', (done) => {
-    //         chai.request(server)
-    //             .get("/product/1?api_key=" + apiKey)
-    //             .end((err, res) => {
-    //                 res.should.have.status(200);
-    //                 res.body.data.should.have.property("name");
-    //                 res.body.data.name.should.be.equal("Big Screw");
-    //                 res.body.data.should.have.property("description");
-    //                 res.body.data.description.should.be.equal("Mighty fine big screw.");
-    //
-    //                 done();
-    //             });
-    //     });
-    // });
-    //
-    // describe('DELETE /product', () => {
-    //     it('should get 400 no id supplied', (done) => {
-    //         let product = {
-    //             api_key: apiKey
-    //         };
-    //
-    //         chai.request(server)
-    //             .delete("/product?api_key=" + apiKey)
-    //             .send(product)
-    //             .end((err, res) => {
-    //                 res.should.have.status(400);
-    //                 res.body.should.be.an("object");
-    //                 res.body.should.have.property("errors");
-    //                 res.body.errors.should.have.property("status");
-    //                 res.body.errors.status.should.be.equal(400);
-    //                 res.body.errors.should.have.property("detail");
-    //
-    //                 done();
-    //             });
-    //     });
-    //
-    //     it('should get 204 HAPPY PATH', (done) => {
-    //         let product = {
-    //             id: 1,
-    //             api_key: apiKey
-    //         };
-    //
-    //         chai.request(server)
-    //             .delete("/product")
-    //             .send(product)
-    //             .end((err, res) => {
-    //                 res.should.have.status(204);
-    //
-    //                 done();
-    //             });
-    //     });
-    //
-    //     it('should get 200 HAPPY PATH getting no products', (done) => {
-    //         chai.request(server)
-    //             .get("/products?api_key=" + apiKey)
-    //             .end((err, res) => {
-    //                 res.should.have.status(200);
-    //                 res.body.should.be.an("object");
-    //                 res.body.data.should.be.an("array");
-    //                 res.body.data.length.should.be.equal(0);
-    //
-    //                 done();
-    //             });
-    //     });
-    // });
+
+    describe('GET /order/search/:query', () => {
+        it('should get 200 HAPPY PATH', (done) => {
+            chai.request(server)
+                .get("/order/search/anders?api_key=" + apiKey)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an("object");
+                    res.body.data.should.be.an("array");
+                    res.body.data.length.should.be.equal(1);
+
+                    done();
+                });
+        });
+
+        it('should get 200, but empty data object', (done) => {
+            chai.request(server)
+                .get("/order/search/bengt?api_key=" + apiKey)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.data.should.be.eql([]);
+
+                    done();
+                });
+        });
+    });
+
+    describe('PUT /order', () => {
+        it('should get 400 no id supplied', (done) => {
+            let order = {
+                name: "Bengt",
+                api_key: apiKey
+            };
+
+            chai.request(server)
+                .put("/order?api_key=" + apiKey)
+                .send(order)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("errors");
+                    res.body.errors.should.have.property("status");
+                    res.body.errors.status.should.be.equal(400);
+                    res.body.errors.should.have.property("detail");
+
+                    done();
+                });
+        });
+
+        it('should get 204 HAPPY PATH', (done) => {
+            let order = {
+                id: 1,
+                name: "Bengt",
+                api_key: apiKey
+            };
+
+            chai.request(server)
+                .put("/order?api_key=" + apiKey)
+                .send(order)
+                .end((err, res) => {
+                    res.should.have.status(204);
+
+                    done();
+                });
+        });
+
+        it('should get 200, with changed name and description', (done) => {
+            chai.request(server)
+                .get("/order/1?api_key=" + apiKey)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.data.should.have.property("name");
+                    res.body.data.name.should.be.equal("Bengt");
+
+                    done();
+                });
+        });
+    });
+
+    describe('DELETE /order', () => {
+        it('should get 400 no id supplied', (done) => {
+            let order = {
+                api_key: apiKey
+            };
+
+            chai.request(server)
+                .delete("/order?api_key=" + apiKey)
+                .send(order)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("errors");
+                    res.body.errors.should.have.property("status");
+                    res.body.errors.status.should.be.equal(400);
+                    res.body.errors.should.have.property("detail");
+
+                    done();
+                });
+        });
+
+        it('should get 204 HAPPY PATH', (done) => {
+            let order = {
+                id: 1,
+                api_key: apiKey
+            };
+
+            chai.request(server)
+                .delete("/order")
+                .send(order)
+                .end((err, res) => {
+                    res.should.have.status(204);
+
+                    done();
+                });
+        });
+
+        it('should get 200 HAPPY PATH getting no orders', (done) => {
+            chai.request(server)
+                .get("/orders?api_key=" + apiKey)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an("object");
+                    res.body.data.should.be.an("array");
+                    res.body.data.length.should.be.equal(0);
+
+                    done();
+                });
+        });
+    });
 });
