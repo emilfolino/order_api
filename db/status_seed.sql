f5a3e2537363de1bc4ccc29e715a1aef08a2bdd5
Emil Folino BTH MAC <emilfolino@gmail.com>
2018-02-11 11:58:08 +0100

    Removed status seed and added status creation back into seed

--- db/status_seed.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO status (id, status, apiKey) VALUES (1, "Ny", "fdc42b2d941e8c6f7b38d974df3758ce");
-INSERT INTO status (id, status, apiKey) VALUES (200, "Packad", "fdc42b2d941e8c6f7b38d974df3758ce");
-INSERT INTO status (id, status, apiKey) VALUES (900, "Skickad", "fdc42b2d941e8c6f7b38d974df3758ce");
