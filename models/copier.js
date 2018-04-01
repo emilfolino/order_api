a51d08299f573ac03e1cee7aeaec4592368b10e7
Emil Folino BTH MAC <emilfolino@gmail.com>
2018-04-01 16:18:21 +0200

    Fixed problem with async sending of response causing the API to crash

--- models/copier.js
+++ models/copier.js
@@ -7,21 +7,6 @@ const copier = (function () {
     const copyApiKey = config.copyApiKey;
 
     function copyAll(res, apiKey) {
-        copyProductsHelper(res, apiKey, false);
-        copyOrdersHelper(res, apiKey, false);
-
-        res.status(201).json({
-            data: {
-                message: "Products and orders have been copied"
-            }
-        });
-    }
-
-    function copyProducts(res, apiKey) {
-        copyProductsHelper(res, apiKey);
-    }
-
-    function copyProductsHelper(res, apiKey, sendResponse=true) {
         let sql = "INSERT INTO products" +
             " (productId," +
             " articleNumber," +
@@ -55,18 +40,113 @@ const copier = (function () {
                     }
                 });
             } else {
-                if (sendResponse) {
-                    products.getAllProducts(res, apiKey, 201);
-                }
+                let sql = "INSERT INTO orders" +
+                    " (orderId," +
+                    " customerName," +
+                    " customerAddress," +
+                    " customerZip," +
+                    " customerCity," +
+                    " customerCountry," +
+                    " statusId," +
+                    " apiKey)" +
+                    " SELECT orderId," +
+                    " customerName," +
+                    " customerAddress," +
+                    " customerZip," +
+                    " customerCity," +
+                    " customerCountry," +
+                    " statusId," +
+                    "'" + apiKey + "'" +
+                    " FROM orders" +
+                    " WHERE apiKey = ?";
+
+                db.run(sql, copyApiKey, (err) => {
+                    if (err) {
+                        return res.status(500).json({
+                            errors: {
+                                status: 500,
+                                source: "/copy_orders",
+                                title: "Database error",
+                                detail: err.message
+                            }
+                        });
+                    } else {
+                        let orderItemsSQL = "INSERT INTO order_items" +
+                            " (orderId," +
+                            " productId," +
+                            " amount," +
+                            " apiKey)" +
+                            " SELECT orderId," +
+                            " productId," +
+                            " amount," +
+                            "'" + apiKey + "'" +
+                            " FROM order_items" +
+                            " WHERE apiKey = ?";
+
+                        db.run(orderItemsSQL, copyApiKey, (err) => {
+                            if (err) {
+                                return res.status(500).json({
+                                    errors: {
+                                        status: 500,
+                                        source: "/copy_orders",
+                                        title: "Database error in order_items",
+                                        detail: err.message
+                                    }
+                                });
+                            } else {
+                                return res.status(201).json({
+                                    data: {
+                                        message: "Products and orders have been copied"
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    function copyProducts(res, apiKey) {
+        let sql = "INSERT INTO products" +
+            " (productId," +
+            " articleNumber," +
+            " productName," +
+            " productDescription," +
+            " productSpecifiers," +
+            " stock," +
+            " location," +
+            " price," +
+            " apiKey)" +
+            " SELECT productId," +
+            " articleNumber," +
+            " productName," +
+            " productDescription," +
+            " productSpecifiers," +
+            " stock," +
+            " location, " +
+            " price," +
+            "'" + apiKey + "'" +
+            " FROM products" +
+            " WHERE apiKey = ?";
+
+        db.run(sql, copyApiKey, (err) => {
+            if (err) {
+                return res.status(500).json({
+                    errors: {
+                        status: 500,
+                        source: "/copy_products",
+                        title: "Database error",
+                        detail: err.message
+                    }
+                });
+            } else {
+                products.getAllProducts(res, apiKey, 201);
             }
         });
     }
 
     function copyOrders(res, apiKey) {
-        copyOrdersHelper(res, apiKey);
-    }
-
-    function copyOrdersHelper(res, apiKey, sendResponse=true) {
         let sql = "INSERT INTO orders" +
             " (orderId," +
             " customerName," +
@@ -121,9 +201,7 @@ const copier = (function () {
                             }
                         });
                     } else {
-                        if (sendResponse) {
-                            orders.getAllOrders(res, apiKey, 201);
-                        }
+                        orders.getAllOrders(res, apiKey, 201);
                     }
                 });
             }
