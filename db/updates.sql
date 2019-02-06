033fc49bb3d49ae16d93efd77144481b6e2122bf
Emil Folino <emilfolino@gmail.com>
2019-02-06 14:33:40 +0100

    Equalityfied the base orders

---
+++ db/updates.sql
@@ -0,0 +1,3 @@
+UPDATE orders SET customerName="Nora Norrby", customerAddress="Norasgatan 3", customerZip="34567", customerCity="Nora", customerCountry="Sweden" WHERE ROWID=3;
+
+UPDATE orders SET customerName="Ulla Ullman", customerAddress="Ullasallén 4", customerZip="45678", customerCity="Ullared", customerCountry="Sweden" WHERE ROWID=4;
