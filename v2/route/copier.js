3c8e2340d7d8f6a9fd1c2d20cd4dced5a20468c0
Emil Folino <emilfolino@gmail.com>
2025-10-01 20:31:09 +0200

    Removed /copier/all and /copier/reset due to new course

--- v2/route/copier.js
+++ v2/route/copier.js
@@ -8,8 +8,8 @@ router.post('/products', (req, res) => copier.copyProducts(
     req.body.api_key
 ));
 
-router.post('/all', (req, res) => copier.copyAll(res, req.body.api_key));
+// router.post('/all', (req, res) => copier.copyAll(res, req.body.api_key));
 
-router.post('/reset', (req, res) => copier.reset(res, req.body.api_key));
+// router.post('/reset', (req, res) => copier.reset(res, req.body.api_key));
 
 module.exports = router;
